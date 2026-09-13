067f2670120194606aed28ab207605afc7f9cf2c
George <gulubovgeorge@gmail.com>
2026-09-13 20:07:43 +0300

    chore: move BrowserRouter to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from './components/ui/provider.jsx'
@@ -7,7 +8,9 @@ import { Provider } from './components/ui/provider.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
