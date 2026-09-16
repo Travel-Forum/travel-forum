bf8e60456b6f9bd8e4ec9f35f11b0f7b98b0288f
George <gulubovgeorge@gmail.com>
2026-09-16 12:09:38 +0300

    created schemas with validations for sign in/up

---
+++ client/src/schemas/authSchemas.js
@@ -0,0 +1,34 @@
+import { z } from 'zod'
+
+export const signUpSchema = z.object({
+
+  firstName: z.string()
+    .min(4, "Minimum 4 symbols")
+    .max(32, "Maximum 32 symbols"),
+
+  lastName: z.string()
+    .min(4, "Minimum 4 symbols")
+    .max(32, "Maximum 32 symbols"),
+
+  username: z.string()
+    .min(4, "Minimum 4 symbols")
+    .max(32, "Maximum 32 symbols"),
+
+  email: z.string()
+    .email("Email is not valid"),
+
+  phone: z.e164("Invalid phone number format"),
+
+  password: z.string()
+    .min(8, "Password need to be minimum 8 symbols"),
+  confirmPassword: z.string(),
+
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Passwords do not match",
+  path: ["confirmPassword"],
+})
+
+export const signInSchema = z.object({
+  email: z.string().email("Email is not valid"),
+  password: z.string().min(1, "Password is required"),
+})
