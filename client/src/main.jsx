4c68ea43728466ca858b6684d2266485812c0bc9
George <gulubovgeorge@gmail.com>
2026-09-16 13:09:29 +0300

    add Toaster for rendering

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from './components/ui/provider.jsx'
+import { Toaster } from './components/ui/toaster.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider defaultTheme="light">
       <BrowserRouter>
         <App />
+        <Toaster />
       </BrowserRouter>
     </Provider>
   </StrictMode>,
