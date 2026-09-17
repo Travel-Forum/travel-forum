4f526b2caed4ff2c0f4d4eb00798a60090674942
George <gulubovgeorge@gmail.com>
2026-09-17 13:41:03 +0300

    created route for auth callback in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import SignUp from "./components/Auth/SignUp/SignUp";
 import SignIn from "./components/Auth/SignIn/SignIn";
+import AuthCallback from "./components/Auth/AuthCallback/AuthCallback";
 import Profile from "./pages/profile/Profile";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/SignUp" element={<SignUp />} />
       <Route path="/SignIn" element={<SignIn />} />
+      <Route path="/auth/callback" element={<AuthCallback />} />
       <Route path="/profile" element={<Profile />} />
     </Routes>
   );
