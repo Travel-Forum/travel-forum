fd6159631405151aa5d680a078675db97428501b
Kristela K <kristela.kyulevchieva@gmail.com>
2026-09-18 11:13:02 +0300

    Add navbar icons and forum logo

--- client/src/components/Navbar/NavbarDesign.jsx
+++ client/src/components/Navbar/NavbarDesign.jsx
@@ -1,15 +1,24 @@
-import { Flex, Box, Input, Text } from '@chakra-ui/react';
+import { Flex, Box, Input, HStack, IconButton, Image} from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import { LuHome, LuMapPin, LuBell } from 'react-icons/lu';
+import forumLogo from '../../assets/icons/Forum logo.svg';
 
 function NavbarDesign() {
     return (
         <Flex as="nav" align="center" gap={4} px={{base:4, md: 6}} borderBottomWidth="1px">
-            <Link to="/"><Text fontWeight="bold">logo</Text></Link>
+           <Link to="/"><Image src={forumLogo} alt="Travel Forum logo" h="40px" /></Link>
 
             <Box flex="1" maxW="500px">
                 <Input placeholder="Search"/>
             </Box>
+
+            <HStack gap={2}>
+                <Link to="/"><IconButton aria-label="Home" variant="ghost"><LuHome /></IconButton></Link>
+                <Link to="/locations"><IconButton aria-label="Locations" variant = "ghost"><LuMapPin /></IconButton></Link>
+                <IconButton aria-label="Notifications" variant="ghost"><LuBell /></IconButton>
+            </HStack>
         </Flex>
-    )
+    );
+
 }
 export default NavbarDesign;
