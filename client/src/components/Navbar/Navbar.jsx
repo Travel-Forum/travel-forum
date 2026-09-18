c07488a1fd24fd58ef23b00788fa8a15611b2a9a
Kristela K <kristela.kyulevchieva@gmail.com>
2026-09-18 11:24:41 +0300

    Add route-based logic to Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
+import { useLocation } from 'react-router-dom';
 import NavbarDesign from 'NavbarDesign';
 
 function Navbar() {
-    return <NavbarDesign />;
+    const { pathname } = useLocation();
+    const isAuthPage = pathname === '/signin' || pathname === '/signup';
+    return <NavbarDesign isAuthPage={isAuthPage} />;
 }
 
 export default Navbar;
