e784d607cbdc46d07cfd9330e9224d1a86fbfa50
George <gulubovgeorge@gmail.com>
2026-09-19 20:53:48 +0300

    remove handle sign out function and useNavigate

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,24 +1,17 @@
-import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth.js';
 
 import Navbar from '../../components/Navbar/Navbar.jsx';
+import Loading from '../../components/Ui/Loading.jsx';
+
 
 const Profile = () => {
-  const { user, loading, signOut } = useAuth();
-  const navigate = useNavigate();
+  const { loading } = useAuth();
 
-  const HandleSignOut = async () => {
-    await signOut();
-    navigate("/");
-  }
-
-  if (loading) return <div>Loading...</div>;
+  if (loading) return <Loading />;
 
   return (
     <div>
       <Navbar />
-      <h1>Welcome, {user?.email}</h1>
-      <button onClick={HandleSignOut}>Sign Out</button>
     </div>
   );
 };
