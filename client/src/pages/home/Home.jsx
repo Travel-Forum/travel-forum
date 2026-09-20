0393774647366e881cbb8a4a71bc104c9fdbc426
Kristela K <kristela.kyulevchieva@gmail.com>
2026-09-20 13:13:42 +0300

    feat(home): add Hero section with light teal accent

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,14 +1,14 @@
 import { Box, Container } from "@chakra-ui/react";
 import Navigation from '../../components/Navigation/Navigation';
-
+import Hero from "./sections/Hero";
 const Home = () => {
 
     return (
-        <Box minH="100vh">
+        <Box minH="100vh" colorPalette="blue">
           <Navigation />
 
           <Container maxW="6xl" py={8}>
-            <Box as="section" py={10} textAlign="center">Hero (TODO)</Box>
+            <Hero />
             <Box as="section" py={6}>Stats (TODO)</Box>
             <Box as="section" py={6}>Posts carousel (TODO)</Box>
             <Box as="section" py={10}>Key features (TODO)</Box>
