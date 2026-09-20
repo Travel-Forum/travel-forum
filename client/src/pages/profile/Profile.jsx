72da9f600d83437078b11405dee798d4ad39c0c0
George <gulubovgeorge@gmail.com>
2026-09-20 11:24:58 +0300

    implement basic design for profile card

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -2,6 +2,7 @@ import { useAuth } from '../../hooks/useAuth.js';
 
 import Navbar from '../../components/Navbar/Navbar.jsx';
 import Loading from '../../components/Ui/Loading.jsx';
+import ProfileCard from '../../components/Profile/ProfileCard/ProfileCard.jsx';
 
 
 const Profile = () => {
@@ -12,6 +13,7 @@ const Profile = () => {
   return (
     <div>
       <Navbar />
+      <ProfileCard />
     </div>
   );
 };
