2a7b1b2b7c37b96876774b7aaaf64235deb96bad
George <gulubovgeorge@gmail.com>
2026-09-20 12:01:44 +0300

    set container from 3 columns

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,3 +1,4 @@
+import { Flex, Grid, GridItem, VStack } from '@chakra-ui/react';
 import { useAuth } from '../../hooks/useAuth.js';
 
 import Navbar from '../../components/Navbar/Navbar.jsx';
@@ -39,15 +40,37 @@ const Profile = () => {
   if (loading) return <Loading />;
 
   return (
-    <div>
+    <Flex direction="column" h="100vh">
       <Navbar />
-      <ProfileCard />
-      <CreatePostTrigeer />
-      {mockPosts.map((post) => (
-        <PostCard key={post.id} post={post} />
-      ))}
-      <ChatBotWidget />
-    </div>
+      <Grid
+        templateColumns="1fr 2fr 1fr"
+        gap={6}
+        maxW="1200px"
+        w="full"
+        mx="auto"
+        p={4}
+        pt={6}
+        flex="1"
+        minH="0"
+      >
+        <GridItem overflowY="auto">
+          <ProfileCard />
+        </GridItem>
+
+        <GridItem overflowY="auto">
+          <VStack gap={4} align="stretch">
+            <CreatePostTrigeer />
+            {mockPosts.map((post) => (
+              <PostCard key={post.id} post={post} />
+            ))}
+          </VStack>
+        </GridItem>
+
+        <GridItem overflowY="auto">
+          <ChatBotWidget />
+        </GridItem>
+      </Grid>
+    </Flex>
   );
 };
 
